f88045b2fee42d19c251b487e8e2169f843789cd
doomj08 <jltinjaca@gmail.com>
2022-06-02 14:44:18 -0500

    adición de conexión a  wifi
    Adición de función WIFI

--- TurcoIOT/src/main.cpp
+++ TurcoIOT/src/main.cpp
@@ -4,12 +4,19 @@
 #include <ESP_Mail_Client.h>
 
 DHT dht;
-#define WIFI_SSID "<ssid>"
-#define WIFI_PASSWORD "<password>"
+#define WIFI_SSID "REVI AUTOS CLARO"
+#define WIFI_PASSWORD "Reviautos"
+
+//************************
+//** F U N C I O N E S ***
+//************************
+void setup_wifi();
 
 void setup(){
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Serial.println("Conectando a internet");
+  setup_wifi();
 
   dht.setup(2); // data pin 2
 }
@@ -22,5 +29,31 @@ void loop(){
   Serial.println("Temperatura");
   Serial.print(dht.getTemperature());
   Serial.print("\t");
+}
+
+void setup_wifi(){
+  delay(5000);
+  // Nos conectamos a nuestra red Wifi
+  Serial.println();
+  Serial.print("Conectando a ssid: ");
+  Serial.println(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  int cont=0;
+  while (WiFi.status() != WL_CONNECTED) {
+    if (cont>10)
+    {
+      cont=0;
+      Serial.println("");  
+      Serial.print("Conectando");
+    }
+    cont++;
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Conectado a red WiFi!");
+  Serial.println("Dirección IP: ");
+  Serial.println(WiFi.localIP());
 }
 
